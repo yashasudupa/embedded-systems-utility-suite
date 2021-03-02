66ab8097260ef624a00e2f9218cff508af9954fc
Yashas <yashasupa@gmail.com>
2021-03-02 18:21:18 +0100

    Update in the parser

--- Utility/utility.cpp
+++ Utility/utility.cpp
@@ -9,92 +9,42 @@
 #include <string.h>
 #include <map>
 
-void find_length (std::string fileName){
+void parser (std::string fileName){
     
     std::ifstream is (fileName, std::ifstream::binary);
-    if (is) {
 
-        std::string line;
-        std::vector<int> length;
+    std::string line;
+    std::string word;
+    std::vector<std::map<std::string, int>> history;
+    std::map<std::string, int> line_no;
+
+    while (std::getline(is, line)) 
+    {
+        std::stringstream ss(line);
+        std::string token;
+        std::getline(ss, token, ' ');
         
-        while (std::getline(is, line)) {
-            int pos = line.find(':');
-            if(pos == EOF) 
-                length.back() += line.size();
-            else
-                length.push_back(line.size());        
-        }
+        line_no.push_back(token, 1);
 
-        std::ifstream is (fileName, std::ifstream::binary);
-        if (is)
-            std::cout << "all characters read successfully." << std::endl;
-        else
-            std::cout << "error: only " << is.gcount() << " could be read";
-        
-        std::vector<int> row_length;
-        row_length.push_back(0);
-        row_length.push_back(length.front() + 1);
-        for (std::vector<int>::iterator it = length.begin() + 1; it != length.end() - 1; ++it){
-            row_length.push_back(row_length.back() + *it + 1);
-        }
+        int pos = token.find(':');
+        if(pos != EOF) 
+            history.push_back({token, 1});     
 
-        std::vector<int>::iterator row_l_it = row_length.begin();
-        std::vector<int>::iterator l_it = length.begin();
-        std::map<std::string, int> history;
-        std::vector<std::string> line_no;
-
-        while(row_l_it != row_length.end() ||
-                l_it != length.end()){
-            std::ifstream is (fileName, std::ifstream::binary);
-            is.seekg (*row_l_it, is.beg);
-            int l = *l_it;
-            
-            char *buffer = new char [l];
-            
-            std::cout << "Reading " << l << " characters... ";
-
-            is.read (buffer,l);
-                    
-            char *token;
-
-            token = strtok(buffer, " ");
-            line_no.push_back(token);
-
-            if(history.empty()){
-                token = strtok(NULL, " ");
-                history.insert({token, 1});
-            }
-            
-            token = strtok(NULL, " ");
-            while(token != NULL) {
-                
-                std::map<std::string, int>::iterator it;
-                for(it = begin(history); it != end(history); ++it){
-                    if (it->first == token){
-                        it->second += 1;
-                        break;
+        while(std::getline(ss, token, ' ')){
+            std::vector<std::map<std::string, int>>::iterator it;
+            for(it = begin(history); it != end(history); ++it)
+            {
+                for(auto& [key, value] : *it){
+                    if(!strcmp(key, token)){
+                        value += 1;
                     }
-                    else if (it == --history.end()){
-                        history.insert({token, 1});
-                        break;
-                    }
+                    else if(key == it->end()->first){
+                        history.back().insert({token, 1});
+                    } 
                 }
-                token = strtok(NULL, " ");
             }
-            
-            std::map<std::string, int>::iterator it;
-            for(it = begin(history); it != end(history); ++it){
-                std::cout << it->first << "\t";
-                std::cout << it->second << std::endl;
-            }
-            is.close();
-
-            delete[] buffer;
-            ++row_l_it;
-            ++l_it;
         }
     }
-    std::cout << "Going out" << std::endl;
 }
 
 int main(int argc, char *argv[]){
@@ -104,7 +54,7 @@ int main(int argc, char *argv[]){
     std::cout << "Enter the file" << std::endl;
     std::cin >> fileName;
 
-    find_length(fileName);
+    parser(fileName);
 
     return 0;
 }
