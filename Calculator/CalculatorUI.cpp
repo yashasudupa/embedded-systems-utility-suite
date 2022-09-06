e015b064ad510b9ef19912f13c462b501736bd17
Yashas Udupa <yashasupa@gmail.com>
2022-09-06 06:40:18 -0700

    Implemented calculator app, going to improve the code as per C++11 features and implement IPC mechanisms

--- Calculator/CalculatorUI.cpp
+++ Calculator/CalculatorUI.cpp
@@ -21,18 +21,23 @@ std::int8_t CalculatorUI::BracketsExtraction(std::regex reg_a, std::string &pars
 
     try
     {
+        std::cout << "CalculatorUI::BracketsExtraction m.str() : " << parsed_string << std::endl;
         std::smatch m;
         if(std::regex_search(InputString, m, reg_a))
         {
             std::smatch temp_m;
-            std::string parsed_string = m.str();
+            parsed_string = m.str();
+            std::cout << "CalculatorUI::BracketsExtraction parsed_string : " << parsed_string.c_str() << std::endl;
+            return 1;
         }
+        std::cout << "CalculatorUI::BracketsExtraction m.str() : " << m.str() << std::endl;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
 
+    return -1;
 }
 
 
@@ -46,14 +51,15 @@ std::int8_t CalculatorUI::OperatorExtraction(std::string parsed_string, std::str
         {
             std::smatch temp_m;
             op = m.str();
+            std::cout << "CalculatorUI::OperatorExtraction op : " << op << std::endl;
             return 1;
         }
-        return -1;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }  
+    return -1;
 }
 
 //Module comments
@@ -69,12 +75,12 @@ std::string CalculatorUI::CalculatorOperation(std::double_t &a, std::double_t &b
         if (op == "+") { result = a+b; return std::to_string(result);};
         if (op == "-") { result = a-b; return std::to_string(result);};
 
-        return "";
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }  
+    return "";
 }
 
 std::int8_t CalculatorUI::ArgumentsExtraction(std::double_t &a, std::double_t &b, std::regex reg_a)
@@ -82,24 +88,36 @@ std::int8_t CalculatorUI::ArgumentsExtraction(std::double_t &a, std::double_t &b
     try
     {
         std::smatch m;
+
         if(std::regex_search(InputString, m, reg_a))
         {
             std::smatch temp_m;
             std::string parsed_string = m.str();
 
-            std::regex reg_first_arg ("[0-9+-*%/]+");
-            std::regex_search(parsed_string, temp_m, reg_first_arg);
+            std::cout << "CalculatorUI::ArgumentsExtraction m.str() : " << m.str() << std::endl;
+            std::regex reg_first_arg ("(((.*)[+-*%/])+");
+            
             a = std::stoi(temp_m.str());
             
             std::regex reg_second_arg ("[+-*%/0-9]+");
-            std::regex_search(parsed_string, temp_m, reg_second_arg);
+            if(!std::regex_search(parsed_string, temp_m, reg_second_arg))
+            {
+                std::cout << "CalculatorUI::ArgumentsExtraction temp_m.str() : " << temp_m.str() << std::endl;
+                return -3;
+            }
             b = std::stoi(temp_m.str());
+
+            std::cout << "a : " << std::to_string(a) << std::endl;
+            std::cout << "b : " << std::to_string(b) << std::endl;
+
+            return 1;
         }
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }        
+    return -1;
 }
 
 std::string CalculatorUI::InititateCalculatorApp(std::string &ipString)
@@ -109,46 +127,53 @@ std::string CalculatorUI::InititateCalculatorApp(std::string &ipString)
     {
         std::string op;
         std::string result;
-        //while(1)
-        //{
-            
-
+        std::int64_t status;
+        while(1)
+        {
             std::string parsed_string;
-            std::regex reg_bracket_1 ("[(0-9^(.*?)0-9)]+");
+            std::regex reg_bracket_1 ("\\((.*)\\)+");
             std::regex reg_bracket_2 ("[{0-9^(.*?)0-9}]+");
             std::regex reg_bracket_3 ("[[0-9^(.*?)0-9]]+");
             std::regex reg_a ("[0-9^(.*?)0-9]+");
 
             std::regex buffer;
             std::smatch m;
-            if(std::regex_search(InputString, m, reg_bracket_1))
+            if(BracketsExtraction(reg_bracket_1, parsed_string) == 1)
             {
-                parsed_string = m.str();
+                std::cout << "reg_bracket_1:m.str() - " << parsed_string << std::endl;
                 buffer = reg_bracket_1;
             }
-            if(std::regex_search(InputString, m, reg_bracket_1))
+            else if(BracketsExtraction(reg_bracket_2, parsed_string) == 1)
             {
-                parsed_string = m.str();
+                std::cout << "reg_bracket_2:m.str() - " << parsed_string << std::endl;
                 buffer = reg_bracket_2;
             }
-            if(std::regex_search(InputString, m, reg_bracket_1))
+            else if(BracketsExtraction(reg_bracket_3, parsed_string) == 1)
             {
-                parsed_string = m.str();
+                std::cout << "reg_bracket_3:m.str() - " << parsed_string << std::endl;
                 buffer = reg_bracket_3;
             }
 
             if (!parsed_string.empty())
             {
-                ArgumentsExtraction(first_argument, second_argument, reg_a);
-                OperatorExtraction(parsed_string, op);
+                status = ArgumentsExtraction(first_argument, second_argument, buffer);
+                if (status != 1) { std::cout << "Invalid input" << std::endl; return "INVALID";}
+
+                status = OperatorExtraction(parsed_string, op);
+                if (status != 1) { std::cout << "Invalid input" << std::endl; return "INVALID";}
+
                 result = CalculatorOperation(first_argument, second_argument, op);
                 std::regex_replace(InputString, buffer, result);
             }
             else
             {
                 ArgumentsExtraction(first_argument, second_argument, reg_a);
+                if (status != 1) { std::cout << "Invalid input" << std::endl; return "INVALID";}
+
                 OperatorExtraction(InputString, op);
-                result = CalculatorOperation(first_argument, second_argument, op);
+                if (status != 1) { std::cout << "Invalid input" << std::endl; return "INVALID";}
+
+                result = CalculatorOperation(first_argument, second_argument, op);   
                 std::regex_replace(InputString, buffer, result);
             }   
             if(result.empty())
@@ -156,14 +181,15 @@ std::string CalculatorUI::InititateCalculatorApp(std::string &ipString)
                 return OutputString;
             }
 
-            OutputString = InititateCalculatorApp(InputString);             
-        //}
+            //OutputString = InititateCalculatorApp(InputString);             
+        }
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
 
+    return "";
 }
 
 void CalculatorUI::GoOnline(void)
@@ -175,6 +201,7 @@ void CalculatorUI::GoOnline(void)
             {
                 std::cin >> InputString;        
                 InititateCalculatorApp(InputString);
+                std::cout << " Enter the input" << std::endl;
             }
             
 
