43c493f3df075d48a7029b4409b574575c2c3332
Yashas <35399149+yashasudupa@users.noreply.github.com>
2025-03-27 17:47:49 +0530

    Updated implementation with better memory management, increased code safety and avoided unintended conversions.

--- Pong game/main.cpp
+++ Pong game/main.cpp
@@ -1,203 +1,130 @@
 #include <iostream>
+#include <vector>
+#include <memory>
 #include <conio.h>
-#include <time.h>
-#include <windows.h>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
-// Enumeration for ball directions
-enum eDir { stop=0, LEFT=1, upleft=2, downleft=3, RIGHT=4, upright=5, downright=6};
+enum class Direction { STOP = 0, LEFT, UPLEFT, DOWNLEFT, RIGHT, UPRIGHT, DOWNRIGHT };
 
-// Class representing the ball
-class cBall {
+class Ball {
 private:
-    int x, y;       // Current position of the ball
-    int origx, origy; // Original position of the ball (reset point)
-    eDir direction;  // Current direction of the ball
+    int x, y;
+    int origX, origY;
+    Direction dir;
 
 public:
-    // Constructor to initialize ball's position
-    cBall(int posx, int posy) {
-        origx = posx;
-        origy = posy;
-        x = posx;
-        y = posy;
-        direction = stop;
-    }
-    
-    // Resets the ball to its original position
-    void reset() {
-        x = origx;
-        y = origy;
-        direction = stop;
-    }
-    
-    // Change the ball's direction
-    void changedir(eDir d) {
-        direction = d;
-    }
-    
-    // Assigns a random direction to the ball
-    void randomdir() {
-        direction = (eDir)((rand() % 6) + 1);
-    }
-    
-    // Getter methods
-    inline int getx() { return x; }
-    inline int gety() { return y; }
-    inline eDir getdir() { return direction; }
-    
-    // Moves the ball based on its current direction
+    Ball(int posX, int posY) : x(posX), y(posY), origX(posX), origY(posY), dir(Direction::STOP) {}
+
+    void reset() { x = origX; y = origY; dir = Direction::STOP; }
+    void setDirection(Direction d) { dir = d; }
+    void randomDirection() { dir = static_cast<Direction>((rand() % 6) + 1); }
+
+    int getX() const { return x; }
+    int getY() const { return y; }
+    Direction getDirection() const { return dir; }
+
     void move() {
-        switch (direction) {
-            case LEFT: x--; break;
-            case RIGHT: x++; break;
-            case upleft: x--; y++; break;
-            case downleft: x--; y--; break;
-            case upright: x++; y++; break;
-            case downright: x++; y--; break;
-            case stop: break;
+        switch (dir) {
+            case Direction::LEFT: x--; break;
+            case Direction::RIGHT: x++; break;
+            case Direction::UPLEFT: x--; y--; break;
+            case Direction::DOWNLEFT: x--; y++; break;
+            case Direction::UPRIGHT: x++; y--; break;
+            case Direction::DOWNRIGHT: x++; y++; break;
+            default: break;
         }
     }
-    
-    // Overloading << operator to print ball info
-    friend ostream & operator<<(ostream & o, cBall c) {
-        o << "Ball [" << c.x << "," << c.y << "][" << c.direction << "]";
-        return o;
-    }
 };
 
-// Class representing the paddle
 class Paddle {
 private:
-    int x, y;       // Current position of paddle
-    int origx, origy; // Original position of paddle (reset point)
+    int x, y;
+    int origX, origY;
+    static constexpr int paddleHeight = 4;
 
 public:
-    // Default constructor
-    Paddle() { x = y = 0; }
-    
-    // Constructor to initialize paddle's position
-    Paddle(int posx, int posy) : Paddle() {
-        origx = posx;
-        origy = posy;
-        x = posx;
-        y = posy;
-    }
-    
-    // Resets the paddle to its original position
-    inline void reset() { x = origx; y = origy; }
-    
-    // Getter methods
-    inline int getx() { return x; }
-    inline int gety() { return y; }
-    
-    // Moves paddle up and down
-    inline void moveup() { y--; }
-    inline void movedown() { y++; }
-    
-    // Overloading << operator to print paddle info
-    friend ostream & operator<<(ostream & o, Paddle c) {
-        o << "Paddle [" << c.x << "," << c.y << "]";
-        return o;
-    }
+    Paddle(int posX, int posY) : x(posX), y(posY), origX(posX), origY(posY) {}
+    void reset() { x = origX; y = origY; }
+    int getX() const { return x; }
+    int getY() const { return y; }
+    int getHeight() const { return paddleHeight; }
+    void moveUp() { if (y > 0) y--; }
+    void moveDown(int height) { if (y + paddleHeight < height) y++; }
 };
 
-// Function to clear the console screen
-void ClearScreen() {
-    COORD cursorPosition;
-    cursorPosition.X = 0;
-    cursorPosition.Y = 0;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
-}
-
-// Class managing the game logic
-class gamemanager {
+class GameManager {
 private:
-    int width, height; // Game area dimensions
-    int score1, score2; // Player scores
-    bool quit; // Flag to exit game
-    char up1, up2, down1, down2; // Player controls
-    cBall *ball; // Ball object
-    Paddle *p1, *p2; // Paddle objects
+    int width, height;
+    int score1, score2;
+    bool quit;
+    char up1, up2, down1, down2;
+    unique_ptr<Ball> ball;
+    unique_ptr<Paddle> p1, p2;
 
 public:
-    // Constructor to initialize game parameters
-    gamemanager(int w, int h) {
-        srand(time(NULL));
-        quit = false;
-        up1 = 'w'; up2 = 'i';
-        down1 = 's'; down2 = 'k';
-        score1 = score2 = 0;
-        width = w;
-        height = h;
-        ball = new cBall(w / 2, h / 2);
-        p1 = new Paddle(1, h / 2 - 3);
-        p2 = new Paddle(w - 2, h / 2 - 3);
-    }
-    
-    // Destructor to free allocated memory
-    ~gamemanager() {
-        delete ball, p1, p2;
-    }
-    
-    // Increases the score for a player and resets ball and paddles
-    void scoreup(Paddle *player) {
-        if (player == p1) score1++;
-        if (player == p2) score2++;
+    GameManager(int w, int h)
+        : width(w), height(h), score1(0), score2(0), quit(false),
+          up1('w'), up2('i'), down1('s'), down2('k'),
+          ball(make_unique<Ball>(w / 2, h / 2)),
+          p1(make_unique<Paddle>(1, h / 2 - 2)),
+          p2(make_unique<Paddle>(w - 2, h / 2 - 2)) {}
+
+    void scoreUp(const Paddle* player) {
+        if (player == p1.get()) score1++;
+        else if (player == p2.get()) score2++;
         ball->reset();
         p1->reset();
         p2->reset();
     }
-    
-    // Draws the game state in console
-    void draw() {
-        ClearScreen();
-        
-        for (int i = 0; i < width + 2; i++) cout << "\xB2";
+
+    void draw() const {
+        system("cls"); // Clear console
+        for (int i = 0; i < width + 2; i++) cout << "#";
         cout << endl;
-        
+
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (j == 0) cout << "\xB2";
-                if (ball->getx() == j && ball->gety() == i) cout << "0";
-                else if (p1->getx() == j && (i >= p1->gety() && i < p1->gety() + 4)) cout << "\xDB";
-                else if (p2->getx() == j && (i >= p2->gety() && i < p2->gety() + 4)) cout << "\xDB";
+                if (j == 0 || j == width - 1) cout << "#";
+                else if (ball->getX() == j && ball->getY() == i) cout << "O";
+                else if (p1->getX() == j && i >= p1->getY() && i < p1->getY() + p1->getHeight()) cout << "|";
+                else if (p2->getX() == j && i >= p2->getY() && i < p2->getY() + p2->getHeight()) cout << "|";
                 else cout << " ";
-                if (j == width - 1) cout << "\xB2";
             }
             cout << endl;
         }
-        for (int i = 0; i < width + 2; i++) cout << "\xB2";
+
+        for (int i = 0; i < width + 2; i++) cout << "#";
         cout << endl;
         cout << "Score 1: " << score1 << " Score 2: " << score2 << endl;
     }
-    
-    // Handles user input for paddle movement and game controls
+
     void input() {
         ball->move();
         if (_kbhit()) {
-            char current = _getch();
-            if (current == up1 && p1->gety() > 0) p1->moveup();
-            if (current == up2 && p2->gety() > 0) p2->moveup();
-            if (current == down1 && p1->gety() + 4 < height) p1->movedown();
-            if (current == down2 && p2->gety() + 4 < height) p2->movedown();
-            if (ball->getdir() == stop) ball->randomdir();
-            if (current == 'q') quit = true;
+            char key = _getch();
+            if (key == up1) p1->moveUp();
+            if (key == up2) p2->moveUp();
+            if (key == down1) p1->moveDown(height);
+            if (key == down2) p2->moveDown(height);
+            if (ball->getDirection() == Direction::STOP) ball->randomDirection();
+            if (key == 'q') quit = true;
         }
     }
-    
-    // Runs the game loop
+
     void run() {
         while (!quit) {
             draw();
             input();
+            this_thread::sleep_for(chrono::milliseconds(100));
         }
     }
 };
 
 int main() {
-    gamemanager c(40, 20);
-    c.run();
+    GameManager game(40, 20);
+    game.run();
     return 0;
 }
